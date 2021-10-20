aef8cd18405936284dc771ea84906bcb4f968a06
PZz <36237190+pzz322@users.noreply.github.com>
2021-10-20 18:31:29 +0300

    Update bot_to_test.py

--- bot_to_test.py
+++ bot_to_test.py
@@ -1,27 +1,26 @@
-import telebot
 from time import sleep
+from telethon.sync import TelegramClient
+ 
+api_id = 'ID_BOT'
+api_hash = 'HASH'
+bot_token = 'API TOKEN'
 
-API_TOKEN = "API_TOKEN"
-
-bot = telebot.TeleBot(API_TOKEN)
-
-def notify(message):
-    command_sender = message.from_user.id
-    with open(r'/path/to/file/chat.txt') as ids:
-        while True:        
-            for line in ids:
-                user_id = int(line.strip("\n"))
-                try:
-                    bot.send_message(user_id,  f'message to {command_sender}')
-                except Exception as e:
-                    bot.send_message(command_sender, f'error sending message - {user_id}')
+def send(text):
+    with TelegramClient('Session', api_id, api_hash).start(bot_token=bot_token) as client:
+        command_sender = text.from_user.id
+        with open(r'/path/to/file/chat.txt') as ids:
+            while True:        
+                for line in ids:
+                    user_id = int(line.strip("\n"))
+                    try:
+                        client.send_message(user_id,  f'message to {command_sender}')
+                    except Exception as e:
+                        client.send_message(command_sender, f'error sending message - {user_id}')
                 sleep(1800)
-        else:
-            bot.send_message(command_sender, f'Not enough rights')
-
-
-if __name__ == "__main__":
-    try:
-        bot.polling(none_stop=True)
-    except Exception as e:
-        pass
+            else:
+                client.send_message(command_sender, f'Not enough rights')
+        client.run_until_disconnected()
+ 
+if __name__ == '__main__':
+    text = input('Enter text ')
+    send(text)
